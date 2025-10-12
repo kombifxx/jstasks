635ed0ca24914887f92f9848e484c0e4e40ead89
kombifxx <arinaghost@gmail.com>
2025-10-12 17:26:54 +0300

    operate with numbers by the string operators

---
+++ arithmetic.js
@@ -0,0 +1,7 @@
+function arithmetic(a, b, operator){
+  return (operator === "add")      ? a + b
+       : (operator === "subtract") ? a - b
+       : (operator === "multiply") ? a * b
+       : (operator === "divide")   ? a / b
+       : undefined; 
+}
